9173004ab11a35fe9ab7bd5ec9d91a5259c024c0
abhimanyu <abhimanyu.vasishth@gmail.com>
2016-09-16 19:08:09 +0400

    replaces some instances of a few strings

--- content.js
+++ content.js
@@ -1,16 +1,43 @@
 console.log("Working on here");
 
 //when tab is active, this is received (form background.js) in its console
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		console.log(request.count);
-		if (request.test == "bg_message") {
-			console.log("got it!")
+// chrome.runtime.onMessage.addListener(
+// 	function(request, sender, sendResponse) {
+// 		console.log(request.count);
+// 		if (request.test == "bg_message") {
+// 			console.log("got it!")
+// 			//call replace text function here
+// 			walkAndObserve(document);
+// 			sendResponse({type: "I got your message"});
+// 		}
+// });
 
+function walk(rootNode){
+    // First replace the text of the title
+    document.title = replaceText(document.title);
 
-			//call replace text function here
+    // Find all the text nodes in rootNode -> which is the document body
+    var walker = document.createTreeWalker(
+        rootNode,
+        NodeFilter.SHOW_TEXT,
+        null,
+        false
+    ),
+    node;
 
+    // Modify each text node's value
+    while (node = walker.nextNode()) {
+    	node.nodeValue = replaceText(node.nodeValue);
+    }
+}	
 
-			sendResponse({type: "I got your message"});
-		}
-	})
+function replaceText(v){
+	v = v.replace("cat","dog");
+	v = v.replace("Cat","Dog");
+	v = v.replace("Cats","Dogs");
+	v = v.replace("cats","dogs");
+	return v;
+}
+
+// This is based on the millennials to snake people chrome extension
+walk(document.body);
